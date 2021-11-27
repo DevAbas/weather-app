6a2c55c65c73c96ba36d2945833a85532663ada2
Abas <thurabli.abas@gmail.com>
2021-11-27 13:22:50 +0400

    Feature/Abas add blue color and poppins font to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#0555BF',
+      },
+      fontFamily: {
+        sans: ["'Poppins'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
