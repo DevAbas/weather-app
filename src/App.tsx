8fef3aa58821a3893010f58e8eeab8530eeec943
Abas <thurabli.abas@gmail.com>
2021-11-27 21:46:37 +0400

    Feature/Abas implement UI design on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { SearchLocation } from './components/SearchLocation';
 import { Logo } from './components/UI/Logo';
 import { WeatherBox } from './components/UI/WeatherBox';
+import { WeatherDetails } from './components/WeatherDetails';
 
 function App() {
   return (
     <WeatherBox>
-      <Logo />
+      <div className='w-full flex justify-between'>
+        <div className='flex flex-col'>
+          <div className='mb-20'>
+            <Logo />
+          </div>
+
+          <div className='w-72'>
+            <SearchLocation />
+          </div>
+
+          <div className='mt-auto'>
+            <h2 className='text-8xl mb-1'>London</h2>
+            <p className='font-light'>November 23, 2021</p>
+          </div>
+        </div>
+        <div>
+          <WeatherDetails />
+        </div>
+      </div>
     </WeatherBox>
   );
 }
